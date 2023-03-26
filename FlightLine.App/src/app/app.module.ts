09c3098f4d1149d221cb8a3818272b495c70b48e
Stone Mitchell <stone.mitchell.45@gmail.com>
2023-03-25 22:13:18 -0700

    search bar, title, action buttons

--- FlightLine.App/src/app/app.module.ts
+++ FlightLine.App/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatRippleModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
