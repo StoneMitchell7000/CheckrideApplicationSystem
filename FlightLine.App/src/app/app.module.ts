52a992f88f52339e79aeb399bc09ddaf72fbaa38
Stone Mitchell <stone.mitchell.45@gmail.com>
2023-04-13 16:49:15 -0700

    fixed app module

--- FlightLine.App/src/app/app.module.ts
+++ FlightLine.App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,20 +10,20 @@ import { AvailabilityComponent } from './availability/availability.component';
 import { FormComponent } from './form/form.component';
 import { LoginComponent } from './login/login.component';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRippleModule } from '@angular/material/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { NgProgressModule } from 'ngx-progressbar';
-import { NgProgressHttpModule } from 'ngx-progressbar/http';
+import { NgProgressModule } from "ngx-progressbar";
+import { NgProgressHttpModule } from "ngx-progressbar/http";
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AccountDialogComponent } from './account-dialog/account-dialog.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatExpansionModule} from "@angular/material/expansion";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatExpansionModule } from "@angular/material/expansion";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import {MatExpansionModule} from "@angular/material/expansion";
     AvailabilityComponent,
     FormComponent,
     LoginComponent,
-    AccountDialogComponent,
+    AccountDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -45,15 +46,19 @@ import {MatExpansionModule} from "@angular/material/expansion";
     MatSelectModule,
     MatTooltipModule,
     HttpClientModule,
-    NgProgressModule,
+    NgProgressModule.withConfig({
+      color: "#ffcb06"
+    }),
     NgProgressHttpModule,
     MatDialogModule,
     MatSnackBarModule,
     MatDatepickerModule,
     ReactiveFormsModule,
-    MatExpansionModule,
+    FormsModule,
+    MatNativeDateModule,
+	MatExpansionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
