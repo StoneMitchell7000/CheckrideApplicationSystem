aa4ae08856bce2868a87019dde8338e263e0fde7
Stone Mitchell <stone.mitchell.45@gmail.com>
2023-04-18 22:33:52 +0000

    Revert "Form Validation"

--- FlightLine.App/src/app/models/stage-schedule-entry.ts
+++ FlightLine.App/src/app/models/stage-schedule-entry.ts
@@ -1,21 +1,21 @@
 export class StageScheduleEntry {
-  public date: Date;
-  public time: string;
-  public ipName: string;
-  public type: string;
-  public unit: string;
-  public hours: number;
-  public ac: string;
-  public result: string;
+    public date: Date;
+    public time: string;
+    public ipName: string;
+    public type: string;
+    public unit: string;
+    public hours: number;
+    public lastActivity: string;
+    public result: string;
 
-  constructor(obj?: any) {
-    this.date = (obj && obj.date) || obj.date || null;
-    this.time = (obj && obj.time) || obj.time || null;
-    this.ipName = (obj && obj.ip_name) || obj.ipName || null;
-    this.type = (obj && obj.type) || obj.type || null;
-    this.unit = (obj && obj.unit) || obj.unit || null;
-    this.hours = (obj && obj.hours) || obj.hours || null;
-    this.ac = (obj && obj.last_activity) || obj.ac || null;
-    this.result = (obj && obj.result) || obj.result || null;
-  }
+    constructor(obj?: any) {
+        this.date = (obj && obj.date) || obj.date || null;
+        this.time = (obj && obj.time) || obj.time || null;
+        this.ipName = (obj && obj.ip_name) || obj.ipName || null;
+        this.type = (obj && obj.type) || obj.type || null;
+        this.unit = (obj && obj.unit) || obj.unit || null;
+        this.hours = (obj && obj.hours) || obj.hours || null;
+        this.lastActivity = (obj && obj.last_activity) || obj.lastActivity || null;
+        this.result = (obj && obj.result) || obj.result || null;
+    }
 }
